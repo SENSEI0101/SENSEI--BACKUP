60a5839aacdade0d36ebdfa4de2ef090887b8c3d
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-04-09 18:51:33 +0600

    Update bayjid2.js

--- scripts/cmds/bayjid2.js
+++ scripts/cmds/bayjid2.js
@@ -12,10 +12,10 @@ category: "no prefix",
 
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "NIROB 🙂") {
+if (event.body && event.body.toLowerCase() === "kakashi2") {
 return message.reply({
 body: " 「【𝙷𝙴𝙻𝙻𝙾 𝙴𝚅𝙴𝚁𝚈𝙾𝙽𝙴】⦿\n \n\n「nirob】\n─ Here is KAKASHI ☠️」",
-attachment: await global.utils.getStreamFromURL("https://i.imgur.com/IN0CM0T.mp4")
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/QbiVGYW.mp4")
 });
 }
 }
